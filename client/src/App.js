8f292efd2c4b5415f55de0eb1f301c39854a11ea
Yao Liang <yaoliang0112@gmail.com>
2023-03-27 10:57:23 +0800

    Create react app

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+
+function App() {
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api/contacts")
+      .then((res) => {
+        res.json();
+        console.log('res');
+        console.log(res);
+      })
+      .then((data) => {
+        setData(data.message);
+        console.log('data');
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
