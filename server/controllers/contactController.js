9219327abe00cb341968b24f9f71372ba0ad2c24
Yao Liang <yaoliang0112@gmail.com>
2023-03-27 10:57:08 +0800

    Task 2.1 2.2 2.3

---
+++ server/controllers/contactController.js
@@ -0,0 +1,112 @@
+// Import contact model
+Contact = require('../models/contactModel');
+// Handle index actions
+exports.index = function (req, res) {
+    Contact.get(function (err, contacts) {
+        if (err) {
+            res.status(400).json({
+                status: "error",
+                message: err,
+            });
+        } else {
+            res.json({
+                status: "success",
+                message: "Contacts retrieved successfully",
+                data: contacts
+            });
+        }
+    });
+};
+// Handle create contact actions
+exports.new = function (req, res) {
+    var contact = new Contact();
+    contact.name = req.body.name ? req.body.name : contact.name;
+    contact.gender = req.body.gender;
+    contact.email = req.body.email;
+    contact.phone = req.body.phone;
+    // save the contact and check for errors
+    contact.save(function (err) {
+        if (err) {
+            res.status(400).json(err);
+        } else {
+            res.json({
+                message: 'New contact created!',
+                data: contact
+            });
+        }
+    });
+};
+// Handle view contact info
+exports.view = function (req, res) {
+    Contact.findById(req.params.contact_id, function (err, contact) {
+        if (err) {
+            console.log(err)
+            res.status(404).send(err);
+        } else if (contact == null) {
+            res.status(404).json({
+                message: 'Contact Not Found',
+                data: { contact_id: (req.params.contact_id) }
+            });
+        } else {
+            res.status(200).json({
+                message: 'Contact details loading..',
+                data: contact
+            });
+        }
+    });
+};
+// Handle update contact info
+exports.update = function (req, res) {
+    Contact.findById(req.params.contact_id, function (err, contact) {
+        if (err) {
+            res.status(400).send(err);
+        } else if (contact == null) {
+            res.status(404).json({
+                message: 'Contact Not Found',
+                data: { contact_id: (req.params.contact_id) }
+            });
+        } else {
+            contact.name = req.body.name ? req.body.name : contact.name;
+            contact.gender = req.body.gender;
+            contact.email = req.body.email;
+            contact.phone = req.body.phone;
+            // save the contact and check for errors
+            contact.save(function (err) {
+                if (err) {
+                    res.status(400).json(err);
+                } else {
+                    res.json({
+                        message: 'Contact Info updated',
+                        data: contact
+                    });
+                }
+            });
+        }
+    });
+};
+// Handle delete contact
+exports.delete = function (req, res) {
+    Contact.findById(req.params.contact_id, function (err, contact) {
+        if (err) {
+            res.status(400).send(err);
+        } else if (contact == null) {
+            res.status(404).json({
+                message: 'Contact Not Found',
+                data: { contact_id: (req.params.contact_id) }
+            });
+        } else {
+            Contact.remove({
+                _id: req.params.contact_id
+            }, function (err, contact) {
+                if (err) {
+                    res.status(400).send(err);
+                } else {
+                    res.json({
+                        status: "success",
+                        message: 'Contact deleted'
+                    });
+                }
+            });
+        }
+    });
+};
---
+++ server/tests/test.js
@@ -0,0 +1,174 @@
+// Import the dependencies for testing
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var app = require('../index')
+
+// Configure chai
+chai.use(chaiHttp);
+chai.should();
+describe("Contacts", () => {
+    const id = '641d527d020da8934b8bb256';
+    describe("GET /api/contacts", () => {
+        // Test to get all contacts record
+        it("should get all contacts record", (done) => {
+            chai.request(app)
+                .get('/api/contacts')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+
+        // Test to get single contact record
+        it("should get a single contact record", (done) => {
+            const id = '641d527d020da8934b8bb256';
+            chai.request(app)
+                .get(`/api/contacts/${id}`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+
+        // Test to get single contact record
+        it("should not get a single contact record", (done) => {
+            const id = '641878284fb93242b5ddfaf7';
+            chai.request(app)
+                .get(`/api/contacts/${id}`)
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    done();
+                });
+        });
+    });
+
+    describe("POST /api/contacts", () => {
+        // Test to add contact successfully 
+        it("should add new contact record", (done) => {
+            const value = {
+                name: "Test Pass",
+                email: "pass@gmail.com",
+                phone: "81234567",
+                gender: "Female"
+            }
+            chai.request(app)
+                .post('/api/contacts')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .send(value)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+
+        // Test to add contact with name missing
+        it("should return error", (done) => {
+            const value = {
+                email: "pass@gmail.com",
+                phone: "81234567",
+                gender: "Female"
+            }
+            chai.request(app)
+                .post('/api/contacts')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .send(value)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    done();
+                });
+        });
+
+        // Test to add contact with email missing
+        it("should return error", (done) => {
+            const value = {
+                name: "Test Pass",
+                phone: "81234567",
+                gender: "Female"
+            }
+            chai.request(app)
+                .post('/api/contacts')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .send(value)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    done();
+                });
+        });
+
+        // Test to add contact with phone missing
+        it("should return error", (done) => {
+            const value = {
+                name: "Test Pass",
+                email: "pass@gmail.com",
+                gender: "Female"
+            }
+            chai.request(app)
+                .get(`/api/contacts`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+    });
+
+    describe("PUT /api/contacts/:contact_id", () => {
+        // Test to update contact email
+        it("should get contact updated", (done) => {
+            const value = {
+                email: "update@gmail.com"
+            }
+            chai.request(app)
+                .put(`/api/contacts/${id}`)
+                .send(value)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+
+        // Test to update contact without email
+        it("should not get contact updated", (done) => {
+            const value = {
+                name: "New name",
+                phone: "912324567",
+                gender: "Female"
+            }
+            chai.request(app)
+                .put(`/api/contacts/${id}`)
+                .send(value)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+    });
+
+    describe("DELETE /api/contacts/:contact_id", () => {
+        // Test to delete existing contact 
+        it("should get contact deleted", (done) => {
+            chai.request(app)
+                .delete(`/api/contacts/${id}`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+
+        // Test to delete contact that is not existing
+        it("should not get contact updated", (done) => {
+            chai.request(app)
+                .delete(`/api/contacts/${id}`)
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    done();
+                });
+        });
+    });
+});
